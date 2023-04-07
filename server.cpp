fa2b3eb10ebb1e03e91a5a9b3c304f483b9f0c5d
Meksto <meksto986@gmail.com>
2023-04-07 16:19:41 +0300

    added spdlog

--- server.cpp
+++ server.cpp
@@ -9,6 +9,7 @@
 #include <atomic>
 #include "IRepka.h"
 #include "command_responsive.h"
+#include <spdlog/spdlog.h>
 
 #define MAX_MSG 256
 static std::atomic<int> active_connections = 0;
@@ -17,7 +18,7 @@ void too_many_connections(int &client_fd) {
     const char buf[16] = "E:TMC";
     std::string msg = "E:TMC";
     if (send(client_fd, buf, sizeof(buf), 0) == -1) {
-            std::cerr << "Failed to send data to client " << client_fd << std::endl;
+            spdlog::error("Failed to send data to client {}", client_fd);
             close(client_fd);
             return;
         }
@@ -30,7 +31,7 @@ bool send_connection_succsess(int& client_fd) {
     std::string cs_msg = "OK";
     int bytes_sent = send(client_fd,cs_msg.c_str(), cs_msg.length(),0);
         if (bytes_sent == -1) {
-            std::cerr << "Failed to sent message to server" << std::endl;
+            spdlog::error("Failed to send data to client {}", client_fd);
             close(client_fd);
             return false;
         }
@@ -39,7 +40,7 @@ bool send_connection_succsess(int& client_fd) {
 
 // function that handles communications with a every single client
 void client_handler(int client_fd, IRepka& repka1) {
-    std::cout << "Client " << client_fd << " connected" << std::endl;
+    spdlog::info("Client: {} connected", client_fd);
     char buf[MAX_MSG] {}; // to recieve data from client
     char msg[MAX_MSG] {}; // to send data to client
 
@@ -51,12 +52,12 @@ void client_handler(int client_fd, IRepka& repka1) {
         memset(buf, 0 , sizeof(buf));
         size_t num_bytes = recv(client_fd, buf, sizeof(buf),0);
         if (num_bytes == -1) {
-            std::cerr << "Failed to receive data from client: " << client_fd << std::endl;
+            spdlog::error("Failed to receive data from client: {}", client_fd);
             close(client_fd);
             return;
         }
-        std::cout << " =Received " << num_bytes << " bytes from client: " << client_fd << std::endl;
-        std::cout << " =" << buf << std::endl;
+        spdlog::info("=Received: {0} bytes from client: {1}", num_bytes, client_fd);
+        spdlog::info("={}", buf);
         
         //chek if client is disconnectd
         if (num_bytes == 0) {
@@ -75,18 +76,17 @@ void client_handler(int client_fd, IRepka& repka1) {
 
 
         if (send(client_fd, msg, sizeof(msg), 0) == -1) {
-            std::cerr << "Failed to send data to client " << client_fd << std::endl;
+            spdlog::error("Failed to send data to client {}", client_fd);
             close(client_fd);
             return;
         }
-        std::cout << "Succsessfully sent response to client: " << client_fd << std::endl;
-        
+        spdlog::info("Succsessfully sent response to client: {}", client_fd);
     }
 
     // code to close Connection
     close(client_fd);
     --active_connections;
-    std::cout << "Client(socket) " << client_fd << " has been disconnected" << std::endl;
+    spdlog::info("Client(socket): {} has been disconnected", client_fd);
     return;
 
 
@@ -95,10 +95,20 @@ void client_handler(int client_fd, IRepka& repka1) {
 
 
 int main(int argc, char* argv[]) {
+      //Log config section
+    spdlog::set_level(spdlog::level::debug); // Установка уровня отладки
+
+
+
+
     if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <port> <maximum_clients>" << std::endl;
+        //std::cerr << "Usage: " << argv[0] << " <port> <maximum_clients>" << std::endl;
+        spdlog::warn("Usage: {0} <port> <maximum_clients>", argv[0]);
         return 0;
     }
+
+  
+
     // Create database
     IRepka repka1;
     // get port and MAX_CONNECTIONS from argv[] and transform it to uint;
@@ -108,7 +118,7 @@ int main(int argc, char* argv[]) {
     // create socket on server (allocate memory for socket and set type);
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) {
-        std::cerr << "Failed to create socket on server-side" << std::endl;
+        spdlog::critical("Failed to create socket on server-side");
         return 1;
     }
 
@@ -119,18 +129,18 @@ int main(int argc, char* argv[]) {
     server_address.sin_port = htons(port); //transform int to TCP/IP bytes number
 
     if (bind(server_fd, reinterpret_cast<sockaddr*>(&server_address),sizeof(server_address)) == -1) {
-        std::cerr << "Failed to bind socket to port " << port << std::endl;
+        spdlog::critical("Failed to bind socket to port {}", port);
         return 1;
     }
 
     //listen for incoming connections
 
     if (listen(server_fd, MAX_CONNECTIONS) == -1) {
-        std::cerr << "Failed to listen for connections" << std::endl;
+        spdlog::critical("Failed to listen for connections");
         return 1;
     }
-    std::cout << "Server started" << std::endl;
-    std::cout << "Listenning for incoming connections on port: " << port << std::endl;
+    spdlog::info("Server started");
+    spdlog::info("Listenning for incoming connections on port: {}", port);
     
     //accept incoming connections and create a new thread for handling
 
@@ -141,7 +151,7 @@ int main(int argc, char* argv[]) {
 
         int client_fd = accept(server_fd, reinterpret_cast<sockaddr*>(&client_address),&client_address_len);
         if (client_fd == -1) {
-            std::cerr << "Failed to accept incoming connection" << std::endl;
+            spdlog::error("Failed to accept incoming connection");
             continue;
         }
 
